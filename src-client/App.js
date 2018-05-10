3b9b335f7d37094c45786e2bb69483bc5c67e5e5
Yev <unstoo@gmail.com>
2018-05-10 12:45:58 +0300

    scaffolding for tables

--- src-client/App.js
+++ src-client/App.js
@@ -1,29 +1,9 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 import { setConfig } from 'react-hot-loader'
-import Quill from 'quill'
-import Toolbar from './Toolbar'
-setConfig({ logLevel: 'debug' })
-
-const quillInit = () => { 
-    window.quill = new Quill('#editor')
-
-    window.quill.on('text-change', function(delta, oldDelta, source) {
-        console.log(delta, oldDelta, source)
-        if (source == 'api') {
-          console.log("An API call triggered this change.");
-        } else if (source == 'user') {
-          console.log("A user action triggered this change.");
-        }
-      });
-}
 
 const App = () => {     
-    console.log(Quill.imports)
-    quillInit()
-    return <div className='container'>
-        <Toolbar />     
-    </div>
+    return <h1>Tada!</h1>
 }
 
 export default hot(module)(App)
